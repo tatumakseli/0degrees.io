22b8a8f13beb4a1f322c584d30845b35c8ca2f83
tatumakseli <teit91@gmail.com>
2018-12-05 13:33:34 +0200

    test

--- src/Projects.js
+++ src/Projects.js
@@ -26,14 +26,12 @@ class Projects extends React.Component {
                     The service then makes suggestions for the most usefull clauses to be considered in this particular case.
                     Personalized examples help to explain what kind real life effect's clauses will have and 
                     user can easily, and without any prior knowledge of the law, make the best decions for her unique case.
-                    
+                    </p>
                     {/* Visual and clever design ensures that all of the necessary facts are gathered from the user and therefore the service can algorithmically make suggestions for the most sound solution.  */}
                     {/* Suggestion are presented in a way that everyone understands without a prior knowledge of the law. */}
                     {/* With visual and clever service design user is being asked all the legally relevant questions in way that everyone understands.   */}
                     {' '}
 
-                    <br/>
-                    <br/>
                     {/* In very complex estate planning situations the service can be paired with a lawyer. 
                     As the service captures information about the will creator’s legal inheritors and family situation, 
                     the types of property she has and how she would like to have her inheritance to be distributed, 
@@ -44,6 +42,11 @@ class Projects extends React.Component {
                     to aiding non-profits in encouraging people to give charitable gifts in their last wills.
                     <br/>
                     <br/> */}
+                    <p style={{paddingLeft: '30px', paddingRight: '30px', fontSize: '0.75em'}}><i>For example, service recognises user's want to leave an apartment to his partner but there is also children and granchildren.
+                    Service than suggests that the apartment could be left to the partner only with limited rights and the ownership would be left to descendants.
+                    User is being explained what this arrangment would mean in real life and how much could be saved in taxation.
+                    </i></p>
+                    <p style={{paddingRight: '20px', paddingLeft: '20px'}}>
                     Registration of EU Community design is pending.
                 </p>
                 </div>
@@ -58,9 +61,10 @@ class Projects extends React.Component {
                     {/* We are currently doing research and developing multiple products in the space of legal services. */}
                     We create a new market of legal services by giving individuals and SMEs access to superior quality digital legal services.
                     We solve the problem of needing a lawyer by applying design thinking and principles.
-                    Users are guided through a carefully designed service that makes sure that the legally irrelevant facts are left out and the legally relevant ones
+                    Users are guided through a carefully designed service which makes sure that the legally relevant facts
                     are categorised. This information is used to algoritmically make suggestion for the most sound course of action.
-{/*                     
+{/*                 
+    the legally irrelevant facts are left out and 
                     We are confident that providing legal services to our focus group, individuals and SMEs, requires much more than just raw knowledge of the law. 
                     Our approach is to sort out the  by guiding the user 
                     Using this process we have legally relevant pieces of information categorised and and extract all of the legally valid facts
